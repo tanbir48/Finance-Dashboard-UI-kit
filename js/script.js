a3d54d295ee69981f9bdcc4b946b8afd9aad4a66
Tanbir Hasan <mdtanbir576@gmail.com>
2022-01-03 10:42:10 +0600

    commit

--- js/script.js
+++ js/script.js
@@ -60,5 +60,14 @@ const hamburger = document.getElementById("mobile-button");
 var navmenu = document.getElementById("my-task");
 
 hamburger.addEventListener("click", () => {
-  navmenu.classList.toggle("show");
+  navmenu.style.left = "0%";
+  navmenu.style.transition = "0.5s";
+});
+
+const hamburgercross = document.getElementById("mobile-cross-button");
+var navmenu = document.getElementById("my-task");
+
+hamburgercross.addEventListener("click", () => {
+  navmenu.style.left = "-50%";
+  navmenu.style.transition = "0.5s";
 });
